0e1630bb4a24faf7d82a49e3c1662cb0bc32c204
S H <sangdo719@gmail.com>
2024-03-11 22:47:39 -0700

    add admob

--- app/src/main/java/com/sangdo/whattoeat/MainActivity.kt
+++ app/src/main/java/com/sangdo/whattoeat/MainActivity.kt
@@ -10,11 +10,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.android.gms.ads.MobileAds
 import com.sangdo.whattoeat.ui.theme.WhatToEatTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             WhatToEatTheme {
                 // A surface container using the 'background' color from the theme
