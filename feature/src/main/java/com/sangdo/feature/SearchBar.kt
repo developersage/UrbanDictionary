d1fecc028447e02ae142ac1dd8900fd4b70746f7
S H <sangdo719@gmail.com>
2024-05-04 16:44:31 -0700

    improve SearchBar

--- feature/src/main/java/com/sangdo/feature/SearchBar.kt
+++ feature/src/main/java/com/sangdo/feature/SearchBar.kt
@@ -1,11 +1,17 @@
 package com.sangdo.feature
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -15,6 +21,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.key.onKeyEvent
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sangdo.feature.ui.theme.PurpleGrey40
+import com.sangdo.feature.ui.theme.PurpleGrey80
 
 @Composable
 fun SearchBar(
@@ -22,23 +36,50 @@ fun SearchBar(
     modifier: Modifier = Modifier
 ) {
     var result by remember { mutableStateOf("") }
+    val focusManager = LocalFocusManager.current
+
     Row(
         modifier = modifier
             .fillMaxWidth()
-            .wrapContentHeight(),
+            .wrapContentHeight()
+            .background(color = PurpleGrey80),
         horizontalArrangement = Arrangement.SpaceEvenly,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        TextField(
+        OutlinedTextField(
             value = result,
             onValueChange = { result = it },
+            label = { Text(text = "Search") },
+            leadingIcon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.baseline_search_24),
+                    contentDescription = "Search"
+                )
+            },
             singleLine = true,
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
             keyboardActions = KeyboardActions(
-                onSearch = { onSearch(result) }
-            )
+                onSearch = {
+                    onSearch(result)
+                    focusManager.clearFocus()
+                }
+            ),
+            modifier = modifier
+                .padding(5.dp)
+                .weight(1f)
+                .onKeyEvent { keyEvent ->
+                    if (keyEvent.nativeKeyEvent.keyCode == android.view.KeyEvent.KEYCODE_ENTER) {
+                        onSearch(result)
+                        focusManager.clearFocus()
+                    }
+                    true
+                }
         )
-        Button(onClick = { onSearch(result) }) {
-            Text(text = "Search")
-        }
     }
+}
+
+@Preview
+@Composable
+fun SearchBarPreview() {
+    SearchBar(onSearch = {})
 }
