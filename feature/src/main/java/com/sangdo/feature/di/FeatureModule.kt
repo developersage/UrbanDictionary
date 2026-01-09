66270f6122faa66f070661c2c55bcea275c5b807
sangdo <sangdo719@gmail.com>
2026-01-08 21:57:09 -0800

    minor fix 2

--- feature/src/main/java/com/sangdo/feature/di/FeatureModule.kt
+++ feature/src/main/java/com/sangdo/feature/di/FeatureModule.kt
@@ -5,10 +5,10 @@ import com.sangdo.feature.BuildConfig
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.components.SingletonComponent
 
 @Module
-@InstallIn(ViewModelComponent::class)
+@InstallIn(SingletonComponent::class)
 class FeatureModule {
     @Provides
     fun provideAdRequest() = AdRequest.Builder()
